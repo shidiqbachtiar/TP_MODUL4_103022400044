96f554603c685ce55def6960dd11324b8faee835
Shidiq <shidiqbachtiar05@gmail.com>
2026-03-14 21:06:59 +0700

    second commit

---
+++ KodePos.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace TP_MODUL4_103022400044
+{
+    public class KodePos
+    {
+        private Dictionary<string, string> kodePosTable = new Dictionary<string, string>()
+        {
+            { "Batununggal", "40266" },
+            { "Kujangsari", "40287" },
+            { "Mengger", "40267" },
+            { "Wates", "40256" },
+            { "Cijaura", "40287" },
+            { "Jatisari", "40286" },
+            { "Margasari", "40286" },
+            { "Sekejati", "40286" },
+            { "Kebonwaru", "40272" },
+            { "Maleer", "40274" }
+        };
+
+        public string getKodePos(string kelurahan)
+        {
+            if (kodePosTable.ContainsKey(kelurahan))
+            {
+                return kodePosTable[kelurahan];
+            }
+
+            return "Kode pos tidak ditemukan";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TP_MODUL4_103022400044;
+
+KodePos kodePos = new KodePos();
+
+Console.WriteLine("=== TABLE DRIVEN: KODE POS ===");
+Console.WriteLine("Batununggal -> " + kodePos.getKodePos("Batununggal"));
+Console.WriteLine("Kujangsari -> " + kodePos.getKodePos("Kujangsari"));
+Console.WriteLine("Mengger -> " + kodePos.getKodePos("Mengger"));
+Console.WriteLine("Wates -> " + kodePos.getKodePos("Wates"));
+Console.WriteLine("Cijaura -> " + kodePos.getKodePos("Cijaura"));
+Console.WriteLine("Jatisari -> " + kodePos.getKodePos("Jatisari"));
+Console.WriteLine("Margasari -> " + kodePos.getKodePos("Margasari"));
+Console.WriteLine("Sekejati -> " + kodePos.getKodePos("Sekejati"));
+Console.WriteLine("Kebonwaru -> " + kodePos.getKodePos("Kebonwaru"));
+Console.WriteLine("Maleer -> " + kodePos.getKodePos("Maleer"));
